80e5fa22271d7f39d4c01fab82ddcc5cab99e260
larkwiot <22042193+larkwiot@users.noreply.github.com>
2024-11-09 16:27:45 -0600

    we pipeline

--- internal/bookmanager.go
+++ internal/bookmanager.go
@@ -31,7 +31,10 @@ var acceptedFileTypes = []string{
 type BookManager struct {
 	providers     []providers.Provider
 	extractors    []extractors.Extractor
-	workQueue     chan book.Book
+	extractQueue  chan book.Book
+	searchQueue   chan providers.SearchTerms
+	collateQueue  chan []book.BookResult
+	finishQueue   chan book.Book
 	quit          chan struct{}
 	maxCharacters uint
 	maxAttempts   uint
@@ -41,9 +44,10 @@ type BookManager struct {
 	waitGroup     *sync.WaitGroup
 	pb            *progressbar.ProgressBar
 	outputWriter  *util.JsonStreamWriter
+	threads       int
 }
 
-func NewBookManager(conf *config.Config) (*BookManager, error) {
+func NewBookManager(conf *config.Config, threads int) (*BookManager, error) {
 	err := conf.Validate()
 	if err != nil {
 		return nil, err
@@ -60,6 +64,18 @@ func NewBookManager(conf *config.Config) (*BookManager, error) {
 		dryRun:        false,
 		waitGroup:     &sync.WaitGroup{},
 	}
+
+	if threads == 0 {
+		bm.threads = bm.bestThreadCount()
+	}
+	if threads > 2000 {
+		bm.threads = 2000
+	}
+
+	bm.extractQueue = make(chan book.Book, bm.threads/2)
+	bm.searchQueue = make(chan providers.SearchTerms, bm.threads/2)
+	bm.collateQueue = make(chan []book.BookResult, 10)
+	bm.finishQueue = make(chan book.Book, 10)
 
 	if conf.Tika.Enable {
 		bm.extractors = append(bm.extractors, extractors.NewTikaServer(&conf.Tika))
@@ -88,6 +104,10 @@ func (bm *BookManager) Shutdown() {
 		extractor.Shutdown()
 	}
 	close(bm.quit)
+	close(bm.extractQueue)
+	close(bm.searchQueue)
+	close(bm.collateQueue)
+	close(bm.finishQueue)
 	bm.waitGroup.Wait()
 }
 
@@ -146,12 +166,24 @@ func (bm *BookManager) dispatch() {
 
 	for {
 		select {
-		case bk, isOpen := <-bm.workQueue:
+		case bk, isOpen := <-bm.extractQueue:
 			if !isOpen {
 				return
 			}
 			bm.waitGroup.Add(1)
-			go bm.ingest(&bk)
+			go bm.extract(bk)
+		case srch, isOpen := <-bm.searchQueue:
+			if !isOpen {
+				return
+			}
+			bm.waitGroup.Add(1)
+			go bm.search(srch)
+		case res, isOpen := <-bm.collateQueue:
+			if !isOpen {
+				return
+			}
+			bm.waitGroup.Add(1)
+			go bm.collate(res)
 		case <-bm.quit:
 			return
 		}
@@ -160,75 +192,6 @@ func (bm *BookManager) dispatch() {
 
 func (bm *BookManager) bestThreadCount() int {
 	return runtime.NumCPU() * len(bm.providers) * 2
-}
-
-func (bm *BookManager) ingest(bk *book.Book) {
-	defer bm.waitGroup.Done()
-
-	texts := bm.extract(bk)
-
-	if len(texts) == 0 {
-		bk.ErrorMessage = fmt.Sprintf("book had no texts: %s", bk.Filepath)
-		//log.Println(reason)
-		bm.finishBook(bk)
-		return
-	}
-
-	isbn10s := make([]book.ISBN10, 0)
-	isbn13s := make([]book.ISBN13, 0)
-
-	for _, result := range texts {
-		if result.Error != nil {
-			bk.ErrorMessage = fmt.Sprintf("error: extractor %s failed with: %s", result.Name, result.Error)
-			bm.finishBook(bk)
-			continue
-		}
-
-		isbn10s = append(isbn10s, util.IdentifyIsbn10s(result.Text)...)
-		isbn13s = append(isbn13s, util.IdentifyIsbn13s(result.Text)...)
-	}
-
-	search := providers.SearchTerms{
-		Isbn10s:  isbn10s,
-		Isbn13s:  isbn13s,
-		Filepath: bk.Filepath,
-	}
-
-	if !search.HasAnyTerms() {
-		return
-	}
-
-	results := bm.getBookMetadata(&search)
-
-	bookResults := make([]book.BookResult, 0)
-
-	for _, providerResults := range results {
-		if providerResults.Error != nil {
-			//log.Printf("warning: provider %s failed to get metadata with %s\n", providerName, providerResults.Error)
-			continue
-		}
-
-		identifiedResults := lo.Filter(providerResults.Results, func(result book.BookResult, _ int) bool {
-			return !result.IsUnidentified()
-		})
-
-		bookResults = append(bookResults, identifiedResults...)
-	}
-
-	if len(bookResults) == 0 {
-		return
-	}
-
-	bestOverall, err := book.ChooseBestResult(bookResults)
-	if err != nil {
-		bk.ErrorMessage = fmt.Sprintf("error: could not calculate best total result: %s", err.Error())
-		//log.Println(reason)
-		bm.finishBook(bk)
-		return
-	}
-
-	newBook := bestOverall.ToBook()
-	bm.finishBook(&newBook)
 }
 
 func (bm *BookManager) StartDryRun() {
@@ -253,13 +216,6 @@ func (bm *BookManager) Scan(scanPath string, cache string, threads int, dryRun b
 	if dryRun {
 		bm.StartDryRun()
 		defer bm.EndDryRun()
-	}
-
-	if threads == 0 {
-		threads = bm.bestThreadCount()
-	}
-	if threads > 2000 {
-		threads = 2000
 	}
 
 	log.Printf("book manager: preparing to scan with %d threads\n", threads)
@@ -300,8 +256,6 @@ func (bm *BookManager) Scan(scanPath string, cache string, threads int, dryRun b
 		bm.outputWriter.Input <- makeJsonStreamItem(&bk)
 	}
 
-	bm.workQueue = make(chan book.Book, threads)
-
 	bm.waitGroup.Add(1)
 	go bm.dispatch()
 
@@ -328,22 +282,20 @@ func (bm *BookManager) Scan(scanPath string, cache string, threads int, dryRun b
 				if len(bk.ErrorMessage) != 0 {
 					bm.removeProcessedBook(path)
 					bk.ErrorMessage = ""
-					bm.workQueue <- bk
+					bm.extractQueue <- bk
 				}
 			}
 			//log.Printf("book manager: skipping already-processed %s\n", path)
 			return nil
 		}
 
-		bm.workQueue <- book.Book{Filepath: path}
+		bm.extractQueue <- book.Book{Filepath: path}
 		return nil
 	})
 
 	if err != nil {
 		log.Printf("error: failed to scan %s: %s\n", scanPath, err)
 	}
-
-	close(bm.workQueue)
 
 	bm.waitGroup.Wait()
 
@@ -361,17 +313,84 @@ func (bm *BookManager) importFrom(outputPath string) error {
 	return json.Unmarshal(data, &bm.books)
 }
 
-type extractorResult struct {
-	Name  string
-	Text  string
-	Error error
+func (bm *BookManager) extract(bk book.Book) {
+	defer bm.waitGroup.Done()
+
+	texts := make([]string, 0)
+
+	for _, extractor := range bm.extractors {
+		text, err := extractor.ExtractText(&bk, bm.maxCharacters)
+		if err != nil {
+			log.Printf("error: failed to extract text from %s: %s\n", bk.Filepath, err)
+			continue
+		}
+		texts = append(texts, text)
+	}
+
+	if len(texts) == 0 {
+		bk.ErrorMessage = "no texts extracted"
+		bm.finishQueue <- bk
+		return
+	}
+
+	isbn10s := make([]book.ISBN10, 0)
+	isbn13s := make([]book.ISBN13, 0)
+
+	for _, text := range texts {
+		isbn10s = append(isbn10s, util.IdentifyIsbn10s(text)...)
+		isbn13s = append(isbn13s, util.IdentifyIsbn13s(text)...)
+	}
+
+	search := providers.SearchTerms{
+		Isbn10s:  isbn10s,
+		Isbn13s:  isbn13s,
+		Filepath: bk.Filepath,
+	}
+
+	bm.searchQueue <- search
 }
 
-func (bm *BookManager) extract(bk *book.Book) []extractorResult {
-	return lo.Map(bm.extractors, func(extractor extractors.Extractor, _ int) extractorResult {
-		r, err := extractor.ExtractText(bk, bm.maxCharacters)
-		return extractorResult{Name: extractor.Name(), Text: r, Error: err}
-	})
+func (bm *BookManager) search(search providers.SearchTerms) {
+	defer bm.waitGroup.Done()
+
+	if bm.IsDryRun() {
+		return
+	}
+
+	results := make([]book.BookResult, 0)
+
+	for _, provider := range bm.providers {
+		results, err := provider.GetBookMetadata(&search)
+		if err != nil {
+			continue
+		}
+		results = append(results, results...)
+	}
+
+	if len(results) == 0 {
+		bm.finishQueue <- book.Book{
+			Filepath:     search.Filepath,
+			ErrorMessage: "no results found",
+		}
+		return
+	}
+
+	bm.collateQueue <- results
+}
+
+func (bm *BookManager) collate(results []book.BookResult) {
+	defer bm.waitGroup.Done()
+
+	result, err := book.ChooseBestResult(results)
+	if err != nil {
+		bm.finishQueue <- book.Book{
+			Filepath:     results[0].Filepath,
+			ErrorMessage: fmt.Sprintf("could not collate: %s", err.Error()),
+		}
+		return
+	}
+
+	bm.finishQueue <- result.ToBook()
 }
 
 type providerResult struct {
